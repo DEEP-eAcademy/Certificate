10fcfc28fe22ef588d9d52760b8f5e100155a816
Jakub-eAcademy <jakub@deepeacademy.com>
2024-11-19 15:01:18 +0100

    fix: bump version, update responsible info

--- plugin.php
+++ plugin.php
@@ -5,8 +5,8 @@
 declare(strict_types=1);
 
 $id = 'cert';
-$version = '4.0.1';
+$version = '5.0.0';
 $ilias_min_version = '8.0';
 $ilias_max_version = '8.999';
-$responsible = 'studer + raimann ag - Team Custom 1';
-$responsible_mail = 'support-custom1@studer-raimann.ch';
+$responsible = 'Jakub';
+$responsible_mail = 'jakub@deepeacademy.com';
